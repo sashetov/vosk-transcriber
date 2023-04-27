af08c33972b1c24608d04bc081833fa9b2b13fe9
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2023-04-26 23:42:32 -0700

    updating vosk example data model

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ source venv/bin/activate
 sudo venv/bin/python3 -m pip install --no-cache-dir -r requirements.txt
 git clone https://github.com/alphacep/vosk-api
 cd vosk-api/python/example
-wget https://alphacephei.com/kaldi/models/vosk-model-small-en-us-0.3.zip
-unzip vosk-model-small-en-us-0.3.zip
-mv vosk-model-small-en-us-0.3 model
+#wget https://alphacephei.com/kaldi/models/vosk-model-small-en-us-0.3.zip
+#unzip vosk-model-small-en-us-0.3.zip
+#mv vosk-model-small-en-us-0.3 model
+wget https://alphacephei.com/vosk/models/vosk-model-en-us-aspire-0.2.zip
+unzip vosk-model-en-us-aspire-0.2.zip
+mv vosk-model-en-us-aspire-0.2 model
 deactivate
